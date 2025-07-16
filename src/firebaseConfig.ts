3cf838e1d9248fdc1ef5d6168019806ffb2c13e0
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-07-16 13:25:25 +0530

    Initialize Firestore with getFirestore(app)

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,7 +23,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-const db = getDatabase(app);
+const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { auth, googleProvider, db, storage };
